fecc8f2d9bf62da6402f06ddf1e08ae5ec01a262
Karim Abd El-Raouf <KarimRaouf>
2023-10-12 10:12:29 +0300

    refactor route of application

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,12 +1,12 @@
 import 'dart:ffi';
 
+import 'package:bookly_application_course/core/utils/app_router.dart';
 import 'package:bookly_application_course/core/utils/assets.dart';
 import 'package:bookly_application_course/features/constants.dart';
 import 'package:bookly_application_course/features/home/presentation/views/home_view.dart';
 import 'package:bookly_application_course/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:get/get_navigation/get_navigation.dart';
+import 'package:go_router/go_router.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -66,11 +66,13 @@ class _SplashViewBodyState extends State<SplashViewBody>
     Future.delayed(
       const Duration(seconds: 3),
       () {
-        Get.to(
-          () => const HomeView(),
-          transition: Transition.fade,
-          duration: kTransitionDuration,
-        );
+        // Get.to(
+        //   () => const HomeView(),
+        //   transition: Transition.fade,
+        //   duration: kTransitionDuration,
+        // );
+
+        GoRouter.of(context).push(AppRouter.kHomeView);
       },
     );
   }
