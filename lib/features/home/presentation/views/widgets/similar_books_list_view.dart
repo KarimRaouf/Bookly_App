ad52db4247f3e077f7e15a2a51372fa1e42b4c5c
Karim Abd El-Raouf <KarimRaouf>
2023-10-15 10:33:47 +0300

    create similar books list view

--- lib/features/home/presentation/views/widgets/similar_books_list_view.dart
+++ lib/features/home/presentation/views/widgets/similar_books_list_view.dart
@@ -1,17 +1,17 @@
 import 'package:bookly_application_course/features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
 
-class FeaturedBooksListView extends StatelessWidget {
-  const FeaturedBooksListView({super.key});
+class SimilarBooksListView extends StatelessWidget {
+  const SimilarBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.15,
+      height: MediaQuery.of(context).size.height * 0.25,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) => const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8.0),
+          padding: EdgeInsets.symmetric(horizontal: 5.0),
           child: CustomBookImage(),
         ),
         itemCount: 5,
@@ -19,3 +19,4 @@ class FeaturedBooksListView extends StatelessWidget {
     );
   }
 }
+
