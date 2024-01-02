466939bffe0692505775d4b92a12a8f0e31c2869
KarimRaouf <karimzraouf@gmail.com>
2024-01-02 13:38:44 +0200

    create book model

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_application_course/features/home/presentation/views/book_details_view.dart';
 import 'package:bookly_application_course/features/home/presentation/views/home_view.dart';
+import 'package:bookly_application_course/features/search/presentation/views/search_view.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../features/splash/presentation/views/splash_view.dart';
@@ -7,6 +8,7 @@ import '../../features/splash/presentation/views/splash_view.dart';
 abstract class AppRouter {
   static const kHomeView = '/homeView';
   static const kBookDetailsView = '/BookDetailsView';
+  static const kSearchView = '/searchView';
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -16,6 +18,11 @@ abstract class AppRouter {
       GoRoute(
         path: kHomeView,
         builder: (context, state) => const HomeView(),
+      ),
+
+      GoRoute(
+        path: kSearchView,
+        builder: (context, state) => const SearchView(),
       ),
       GoRoute(
         path: kBookDetailsView,
