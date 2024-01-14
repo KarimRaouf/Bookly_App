7fc0e5f2ce45cf49ad29b72586c7062473479fde
KarimRaouf <karimzraouf@gmail.com>
2024-01-14 11:55:41 +0200

    create home repo methods and failure

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,3 +1,10 @@
-abstract class HomeRepo{
+import 'package:bookly_application_course/features/home/data/models/book_model.dart';
+import 'package:dartz/dartz.dart';
 
+import '../../../../core/errors/failures.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+
+  Future<Either<Failure, List<BookModel>>> fetchFeaturesBooks();
 }
